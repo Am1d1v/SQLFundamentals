79e42056f62b957c9ba1b5faf425cd2d14355195
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-01-05 16:12:41 +0300

    Add files via upload

--- publisher.sql
+++ publisher.sql
@@ -1,4 +1,8 @@
-ALTER TABLE book ADD COLUMN fk_publisher_id INT;
+-- add books data
+-- INSERT INTO book
+-- VALUES
+-- (1, 'Title 1', '111', 1),
+-- (2, 'Title 2', '222', 2),
+-- (3, 'Title 3', '333', 3);
 
-ALTER TABLE book 
-ADD CONSTRAINT fk_book_publisher FOREIGN KEY(fk_publisher_id) REFERENCES publisher (publisher_id);
+SELECT * FROM book;
